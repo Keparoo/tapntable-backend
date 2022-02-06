af921dabef6294b530aa603a2eca59f663c32c80
Kep <KepCreations@gmail.com>
2022-02-06 17:49:02 -0500

    Create seed file and update schema

--- tapntable-schema.sql
+++ tapntable-schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE employees (
   is_active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE event (
+CREATE TABLE log_event (
 id SERIAL PRIMARY KEY,
 type VARCHAR(25) NOT NULL
 );
@@ -20,7 +20,7 @@ type VARCHAR(25) NOT NULL
 CREATE TABLE user_logs (
 id SERIAL PRIMARY KEY,
 emp_id INTEGER REFERENCES employees,
-event_id INTEGER REFERENCES event,
+log_event_id INTEGER REFERENCES log_event,
 timestamp TIMESTAMP NOT NULL,
 entity_id INTEGER --eg. item_ordered_id, or check_id
 );
@@ -38,12 +38,12 @@ name VARCHAR(25) NOT NULL
 CREATE TABLE items (
 id SERIAL PRIMARY KEY,
 name VARCHAR(25) NOT NULL,
-description VARCHAR(25) NOT NULL,
-price NUMERIC(8,2),
+description TEXT,
+price NUMERIC(8,2) NOT NULL,
 category_id INTEGER REFERENCES item_category,
 destination_id INTEGER REFERENCES destination,
 count INTEGER,
-is_active BOOLEAN DEFAULT TRUE
+is_active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE check (
@@ -72,7 +72,7 @@ CREATE TABLE item_ordered (
 id SERIAL PRIMARY KEY,
 item_id INTEGER REFERENCES items
 ticket_id INTEGER REFERENCES ticket,
-check_id INTEGER REFERENCES ticket,
+check_id INTEGER REFERENCES check,
 seat_num INTEGER,
 completed_at TIMESTAMP,
 completed_by INTEGER REFERENCES employees,
@@ -83,13 +83,28 @@ item_discount_id INTEGER --This will eventually point to discount table
 
 CREATE TABLE payment_type (
 id SERIAL PRIMARY KEY,
-type VARCHAR(15),
+type VARCHAR(15) NOT NULL,
 );
 
 CREATE TABLE payments (
 id SERIAL PRIMARY KEY,
-check_id INTEGER REFERENCES ticket,
+check_id INTEGER REFERENCES check,
 payment_type_id INTEGER REFERENCES payment_type,
 tip_amt NUMERIC(6,2),
 sub_total NUMERIC(10,2)
+);
+
+CREATE TABLE restaurant_info (
+id VARCHAR(25) PRIMARY KEY,
+restaurant_name VARCHAR(25),
+address VARCHAR(25),
+city VARCHAR(25),
+state VARCHAR(25),
+zip_code VARCHAR(10),
+phone_number VARCHAR(13),
+website VARCHAR(25),
+local_tax_rate NUMERIC(6,3),
+state_tax_rate NUMERIC(6,3),
+federal_tax_rate NUMERIC(6,3),
+week_start_mon BOOLEAN NOT NULL DEFAULT TRUE
 );
---
+++ tapntable-seed.sql
@@ -0,0 +1,99 @@
+--test users have the password "password"
+
+INSERT INTO employee_roles (id, role)
+VALUES (1, 'trainee'), (2, 'dishwasher'), (3, 'maintenance'), (4, 'cook'), (5, 'host'), (6 'server'), (7, 'bartender'), (8, 'head-server'), (9, 'bar-manager'), (10, 'chef') , (11, 'manager'), (12, 'owner');
+
+INSERT INTO employees (id, display_name, password, first_name, last_name, role_id)
+VALUES (1,
+'Server',
+'$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+'Server-First',
+'Server-Last',
+6),
+(2,
+'Bartender',
+'$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+'',
+'Bartender-First',
+'Bartender-Last',
+7),
+(2,
+'Manager',
+'$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+'',
+'Manager-First',
+'Manager-Last',
+7);
+
+INSERT INTO log_event (id, type)
+VALUES (1, 'clock-in'), (2, 'clock-out'), (3, 'cash-out'),(4, 'close-shift'),(5, 'close-day'),(6, 'discount-item'), (7, 'discount-check');
+
+INSERT INTO item_category (id, name)
+VALUES (1, 'Appetizer'),  (2, 'Soup'), (3, 'Salad'), (4, 'Entree'), (5, 'Addition'), (6, 'Desert'), (7, 'Favorites'), (8, 'Beverage'), (9, 'Beer'), (10, 'Wine'), (11, 'Liquor'), (12, 'Carryout'), (13, 'Delivery');
+
+INSERT INTO destination (id, name)
+VALUES (1, 'Kitchen-hot'), (2, 'Kitchen-cold'),(3, 'Bar'), (4, 'No-send');
+
+INSERT INTO items (id, name, description, price, category_id, destination_id)
+VALUES (1,
+'Chicken Wings',
+'A full pound of wings, deep-fried served plain, mild or hot',
+8.99,
+1,
+1),
+(2,
+'Potato Skins',
+'Baked with mozzarella, cheddar & crumbled bacon',
+7.99,
+2,
+1),
+(3,
+'The Angry Angus',
+'A full pound of wings, deep-fried served plain, mild or hot',
+8.99,
+4,
+1),
+(4,
+'Baked Stuffed Haddock',
+'Tender local haddock stuffed w/ scollop & bread crumb stuffing',
+14.99,
+4,
+1),
+(5,
+'Walnut Chicken Salad',
+'Carmelized walnut-encrusted chicken breast over crisp mesclun greens',
+.99,
+3,
+2),
+(6,
+'Coke',
+NULL,
+.99,
+8,
+4),
+(7,
+'Long Island Ice Tea',
+NULL,
+6.99,
+11,
+3),
+(8,
+'Sterling Cabernet Glass',
+NULL,
+4.99,
+10,
+3);
+
+INSERT INTO payment_type (id, type)
+VALUES (1, 'Cash'), (2, 'MC'), (3, 'Visa'), (4, 'Amex'), (5, 'Disc'), (6, 'Apple'), (7, 'Google'), (8, 'Venmo');
+
+INSERT INTO restaurant_info (id, restaurant_name, address, city, state, zip_code, phone_number, website, state_tax_rate)
+VALUES ('1a',
+'The Duck Inn Pub',
+'447 Main St',
+'Hyannis',
+'MA',
+'02601',
+'508-827-7343',
+'www.duckinnpub.com',
+6.25);
