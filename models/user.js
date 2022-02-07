f95d22599868d75cdca117fabc8f631034f1dba9
Kep <KepCreations@gmail.com>
2022-02-06 23:44:01 -0500

    First pass at User model

--- models/user.js
+++ models/user.js
@@ -14,25 +14,25 @@ const { BCRYPT_WORK_FACTOR } = require('../config.js');
 /** Related functions for users. */
 
 class User {
-	/** authenticate user with username, password.
+	/** authenticate user with displayName, password.
    *
-   * Returns { username, first_name, last_name, email, is_admin }
+   * Returns { displayName, first_name, last_name, role, isActive }
    *
    * Throws UnauthorizedError is user not found or wrong password.
    **/
 
-	static async authenticate(username, password) {
+	static async authenticate(displayName, password) {
 		// try to find the user first
 		const result = await db.query(
-			`SELECT username,
+			`SELECT display_name AS "displayName",
                   password,
                   first_name AS "firstName",
                   last_name AS "lastName",
-                  email,
-                  is_admin AS "isAdmin"
+                  role,
+                  is_active AS "isActive"
            FROM users
-           WHERE username = $1`,
-			[ username ]
+           WHERE display_name = $1`,
+			[ displayName ]
 		);
 
 		const user = result.rows[0];
@@ -46,48 +46,48 @@ class User {
 			}
 		}
 
-		throw new UnauthorizedError('Invalid username/password');
+		throw new UnauthorizedError('Invalid displayName/password');
 	}
 
 	/** Register user with data.
    *
-   * Returns { username, firstName, lastName, email, isAdmin }
+   * Returns { displayName, firstName, lastName, role, isActive }
    *
    * Throws BadRequestError on duplicates.
    **/
 
 	static async register({
-		username,
+		displayName,
 		password,
 		firstName,
 		lastName,
-		email,
-		isAdmin
+		role,
+		isActive
 	}) {
 		const duplicateCheck = await db.query(
-			`SELECT username
+			`SELECT display_name
            FROM users
-           WHERE username = $1`,
-			[ username ]
+           WHERE display_name = $1`,
+			[ displayName ]
 		);
 
 		if (duplicateCheck.rows[0]) {
-			throw new BadRequestError(`Duplicate username: ${username}`);
+			throw new BadRequestError(`Duplicate displayName: ${displayName}`);
 		}
 
 		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
 		const result = await db.query(
 			`INSERT INTO users
-           (username,
+           (display_name,
             password,
             first_name,
             last_name,
-            email,
-            is_admin)
+            role,
+            is_active)
            VALUES ($1, $2, $3, $4, $5, $6)
-           RETURNING username, first_name AS "firstName", last_name AS "lastName", email, is_admin AS "isAdmin"`,
-			[ username, hashedPassword, firstName, lastName, email, isAdmin ]
+           RETURNING display_name AS "displayName", first_name AS "firstName", last_name AS "lastName", role, is_active AS "isActive"`,
+			[ displayName, hashedPassword, firstName, lastName, role, isActive ]
 		);
 
 		const user = result.rows[0];
@@ -97,18 +97,18 @@ class User {
 
 	/** Find all users.
    *
-   * Returns [{ username, first_name, last_name, email, is_admin }, ...]
+   * Returns [{ displayName, firstName, lastName, role, isActive }, ...]
    **/
 
 	static async findAll() {
 		const result = await db.query(
-			`SELECT username,
+			`SELECT display_name AS "displayName" ,
                   first_name AS "firstName",
                   last_name AS "lastName",
-                  email,
-                  is_admin AS "isAdmin"
+                  role,
+                  is_active AS "isActive"
            FROM users
-           ORDER BY username`
+           ORDER BY display_name`
 		);
 
 		return result.rows;
@@ -116,38 +116,38 @@ class User {
 
 	/** Given a username, return data about user.
    *
-   * Returns { username, first_name, last_name, is_admin, jobs }
+   * Returns { displayName, firstName, lastName, role, jobs }
    *   where jobs is { id, title, company_handle, company_name, state }
    *
    * Throws NotFoundError if user not found.
    **/
 
-	static async get(username) {
-		const userRes = await db.query(
-			`SELECT username,
-                  first_name AS "firstName",
-                  last_name AS "lastName",
-                  email,
-                  is_admin AS "isAdmin"
-           FROM users
-           WHERE username = $1`,
-			[ username ]
-		);
+	// static async get(displayName) {
+	// 	const userRes = await db.query(
+	// 		`SELECT display_name AS "displayName",
+	//                 first_name AS "firstName",
+	//                 last_name AS "lastName",
+	//                 role,
+	//                 is_active AS "isActive"
+	//          FROM users
+	//          WHERE display_name = $1`,
+	// 		[ displayName ]
+	// 	);
 
-		const user = userRes.rows[0];
+	// 	const user = userRes.rows[0];
 
-		if (!user) throw new NotFoundError(`No user: ${username}`);
+	// 	if (!user) throw new NotFoundError(`No user: ${displayName}`);
 
-		const userApplicationsRes = await db.query(
-			`SELECT a.job_id
-           FROM applications AS a
-           WHERE a.username = $1`,
-			[ username ]
-		);
+	// 	const userApplicationsRes = await db.query(
+	// 		`SELECT a.job_id
+	//          FROM applications AS a
+	//          WHERE a.username = $1`,
+	// 		[ displayName ]
+	// 	);
 
-		user.applications = userApplicationsRes.rows.map((a) => a.job_id);
-		return user;
-	}
+	// 	user.applications = userApplicationsRes.rows.map((a) => a.job_id);
+	// 	return user;
+	// }
 
 	/** Update user data with `data`.
    *
@@ -155,9 +155,9 @@ class User {
    * all the fields; this only changes provided ones.
    *
    * Data can include:
-   *   { firstName, lastName, password, email, isAdmin }
+   *   { firstName, lastName, password, role, isActive }
    *
-   * Returns { username, firstName, lastName, email, isAdmin }
+   * Returns { displayName, firstName, lastName, role, isActive }
    *
    * Throws NotFoundError if not found.
    *
@@ -166,7 +166,7 @@ class User {
    * or a serious security risks are opened.
    */
 
-	static async update(username, data) {
+	static async update(displayName, data) {
 		if (data.password) {
 			data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
 		}
@@ -174,22 +174,22 @@ class User {
 		const { setCols, values } = sqlForPartialUpdate(data, {
 			firstName: 'first_name',
 			lastName: 'last_name',
-			isAdmin: 'is_admin'
+			isActive: 'is_active'
 		});
-		const usernameVarIdx = '$' + (values.length + 1);
+		const displayNameVarIdx = '$' + (values.length + 1);
 
 		const querySql = `UPDATE users 
                       SET ${setCols} 
-                      WHERE username = ${usernameVarIdx} 
-                      RETURNING username,
+                      WHERE display_name = ${displayNameVarIdx} 
+                      RETURNING display_name AS "displayName",
                                 first_name AS "firstName",
                                 last_name AS "lastName",
-                                email,
-                                is_admin AS "isAdmin"`;
-		const result = await db.query(querySql, [ ...values, username ]);
+                                role,
+                                is_active AS "isActive"`;
+		const result = await db.query(querySql, [ ...values, displayName ]);
 		const user = result.rows[0];
 
-		if (!user) throw new NotFoundError(`No user: ${username}`);
+		if (!user) throw new NotFoundError(`No user: ${displayName}`);
 
 		delete user.password;
 		return user;
@@ -197,17 +197,17 @@ class User {
 
 	/** Delete given user from database; returns undefined. */
 
-	static async remove(username) {
+	static async remove(displayName) {
 		let result = await db.query(
 			`DELETE
            FROM users
-           WHERE username = $1
-           RETURNING username`,
-			[ username ]
+           WHERE display_name = $1
+           RETURNING display_name AS "displayName"`,
+			[ displayName ]
 		);
 		const user = result.rows[0];
 
-		if (!user) throw new NotFoundError(`No user: ${username}`);
+		if (!user) throw new NotFoundError(`No user: ${displayName}`);
 	}
 
 	/** Apply for job: update db, returns undefined.
@@ -216,33 +216,33 @@ class User {
    * - jobId: job id
    **/
 
-	static async applyToJob(username, jobId) {
-		const preCheck = await db.query(
-			`SELECT id
-           FROM jobs
-           WHERE id = $1`,
-			[ jobId ]
-		);
-		const job = preCheck.rows[0];
+	// static async applyToJob(username, jobId) {
+	// 	const preCheck = await db.query(
+	// 		`SELECT id
+	//          FROM jobs
+	//          WHERE id = $1`,
+	// 		[ jobId ]
+	// 	);
+	// 	const job = preCheck.rows[0];
 
-		if (!job) throw new NotFoundError(`No job: ${jobId}`);
+	// 	if (!job) throw new NotFoundError(`No job: ${jobId}`);
 
-		const preCheck2 = await db.query(
-			`SELECT username
-           FROM users
-           WHERE username = $1`,
-			[ username ]
-		);
-		const user = preCheck2.rows[0];
+	// 	const preCheck2 = await db.query(
+	// 		`SELECT username
+	//          FROM users
+	//          WHERE username = $1`,
+	// 		[ username ]
+	// 	);
+	// 	const user = preCheck2.rows[0];
 
-		if (!user) throw new NotFoundError(`No username: ${username}`);
+	// 	if (!user) throw new NotFoundError(`No username: ${username}`);
 
-		await db.query(
-			`INSERT INTO applications (job_id, username)
-           VALUES ($1, $2)`,
-			[ jobId, username ]
-		);
-	}
+	// 	await db.query(
+	// 		`INSERT INTO applications (job_id, username)
+	//          VALUES ($1, $2)`,
+	// 		[ jobId, username ]
+	// 	);
+	// }
 }
 
 module.exports = User;
