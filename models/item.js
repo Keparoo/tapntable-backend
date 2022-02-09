da91c81ca264e2634c9bd31de23fe2dc04b05505
Kep <KepCreations@gmail.com>
2022-02-09 12:13:06 -0500

    Implement item model

--- models/item.js
+++ models/item.js
@@ -38,6 +38,153 @@ class Item {
 
 		return item;
 	}
+
+	/** Find all items (optional filter on searchFilters).
+   *
+   * searchFilters (all optional):
+   * - name (will find case-insensitive, partial matches)
+   * - categoryId
+   * - 
+   *
+   * Returns [{ id, name, description, price, category_id, destination_id, count, is_active }, ...]
+   * */
+
+	static async findAll(searchFilters = {}) {
+		let query = `SELECT id,
+                          name,
+                          description,
+                          price,
+                          category_id AS "categoryId",
+                          destination_id AS "destinationId",
+                          count,
+                          is_active AS "isActive"
+                   FROM items`;
+		let whereExpressions = [];
+		let queryValues = [];
+
+		const { name, categoryId } = searchFilters;
+
+		// For each possible search term, add to whereExpressions and queryValues so
+		// we can generate the right SQL
+
+		if (minEmployees !== undefined) {
+			queryValues.push(minEmployees);
+			whereExpressions.push(`num_employees >= $${queryValues.length}`);
+		}
+
+		if (maxEmployees !== undefined) {
+			queryValues.push(maxEmployees);
+			whereExpressions.push(`num_employees <= $${queryValues.length}`);
+		}
+
+		if (name) {
+			queryValues.push(`%${name}%`);
+			whereExpressions.push(`name ILIKE $${queryValues.length}`);
+		}
+
+		if (whereExpressions.length > 0) {
+			query += ' WHERE ' + whereExpressions.join(' AND ');
+		}
+
+		// Finalize query and return results
+
+		query += ' ORDER BY name';
+		const itemsRes = await db.query(query, queryValues);
+		return itemsRes.rows;
+	}
+
+	/** Given an item id, return data about the item.
+     *
+     * Returns { id, name, description, price, category_id, destination_id, count, is_active }
+     *
+     * Throws NotFoundError if not found.
+     **/
+
+	static async get(id) {
+		const itemRes = await db.query(
+			`SELECT id,
+                    name,
+                    description,
+                    price,
+                    category_id AS "categoryId",
+                    destination_id AS "destinationId",
+                    count,
+                    is_active AS "isActive"
+             FROM items
+             WHERE id = $1`,
+			[ id ]
+		);
+
+		const item = itemRes.rows[0];
+
+		if (!item) throw new NotFoundError(`No item: ${id}`);
+
+		// const jobsRes = await db.query(
+		//       `SELECT id, title, salary, equity
+		//        FROM jobs
+		//        WHERE company_handle = $1
+		//        ORDER BY id`,
+		//     [handle],
+		// );
+
+		// company.jobs = jobsRes.rows;
+
+		return item;
+	}
+
+	/** Update item data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain all the
+   * fields; this only changes provided ones.
+   *
+   * Data can include: { name, description, price, category_id, destination_id, count, is_active }
+   *
+   * Returns { id, name, description, price, category_id, destination_id, count, is_active }
+   *
+   * Throws NotFoundError if not found.
+   */
+
+	static async update(id, data) {
+		const { setCols, values } = sqlForPartialUpdate(data, {
+			categoryId: 'category_id',
+			destinationId: 'destination_id'
+		});
+		const idVarIdx = '$' + (values.length + 1);
+
+		const querySql = `UPDATE items 
+                        SET ${setCols} 
+                        WHERE handle = ${idVarIdx} 
+                        RETURNING id, 
+                                  name, 
+                                  description,
+                                  price,
+                                  category_id AS "categoryId", 
+                                  destination_id AS "destinationId"`;
+		const result = await db.query(querySql, [ ...values, handle ]);
+		const company = result.rows[0];
+
+		if (!item) throw new NotFoundError(`No item: ${id}`);
+
+		return item;
+	}
+
+	/** Delete given item from database; returns undefined.
+   *
+   * Throws NotFoundError if item not found.
+   **/
+
+	static async remove(id) {
+		const result = await db.query(
+			`DELETE
+             FROM items
+             WHERE id = $1
+             RETURNING id`,
+			[ id ]
+		);
+		const item = result.rows[0];
+
+		if (!item) throw new NotFoundError(`No item: ${id}`);
+	}
 }
 
 module.exports = Item;
