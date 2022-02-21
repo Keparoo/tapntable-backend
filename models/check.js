98ee41ce5091292f27a24bb723bfefa000ce5540
Kep <KepCreations@gmail.com>
2022-02-20 19:04:11 -0500

    Update payment

--- models/check.js
+++ models/check.js
@@ -7,7 +7,7 @@ const { sqlForPartialUpdate } = require('../helpers/sql');
 /** Related functions for checks. */
 
 class Check {
-	/** Create a check (from data), update db, return new check data.
+  /** Create a check (from data), update db, return new check data.
    *
    * data should be { userId, tablNum, customer, numGuests }
    *
@@ -19,9 +19,9 @@ class Check {
    * 
    * */
 
-	static async create({ userId, tableNum, numGuests, customer }) {
-		const result = await db.query(
-			`INSERT INTO checks
+  static async create({ userId, tableNum, numGuests, customer }) {
+    const result = await db.query(
+      `INSERT INTO checks
        (user_id, table_num, num_guests, customer)
        VALUES ($1, $2, $3, $4)
        RETURNING id,
@@ -39,14 +39,14 @@ class Check {
                 state_tax AS "stateTax",
                 federal_tax AS "federalTax",
                 is_void AS "isVoid"`,
-			[ userId, tableNum, numGuests, customer ]
-		);
-		const check = result.rows[0];
+      [ userId, tableNum, numGuests, customer ]
+    );
+    const check = result.rows[0];
 
-		return check;
-	}
+    return check;
+  }
 
-	/** Find all checks (optional filter on searchFilters).
+  /** Find all checks (optional filter on searchFilters).
    *
    * searchFilters (all optional):
    * - userId 
@@ -64,8 +64,8 @@ class Check {
    * 
    * */
 
-	static async findAll(searchFilters = {}) {
-		let query = `SELECT c.id,
+  static async findAll(searchFilters = {}) {
+    let query = `SELECT c.id,
                         c.user_id AS "userId",
                         u.display_name AS "employee",
                         c.table_num AS "tableNum",
@@ -83,87 +83,87 @@ class Check {
                         c.is_void AS "isVoid"
                  FROM checks c INNER JOIN users u
                  ON c.user_id = u.id`;
-		let whereExpressions = [];
-		let queryValues = [];
+    let whereExpressions = [];
+    let queryValues = [];
 
-		const {
-			userId,
-			employee,
-			tableNum,
-			numGuests,
-			customer,
-			createdAt,
-			printedAt,
-			closedAt,
-			discountId,
-			isVoid
-		} = searchFilters;
+    const {
+      userId,
+      employee,
+      tableNum,
+      numGuests,
+      customer,
+      createdAt,
+      printedAt,
+      closedAt,
+      discountId,
+      isVoid
+    } = searchFilters;
 
-		// For each possible search term, add to whereExpressions and queryValues so
-		// we can generate the right SQL
+    // For each possible search term, add to whereExpressions and queryValues so
+    // we can generate the right SQL
 
-		if (userId) {
-			queryValues.push(userId);
-			whereExpressions.push(`c.user_id = $${queryValues.length}`);
-		}
+    if (userId) {
+      queryValues.push(userId);
+      whereExpressions.push(`c.user_id = $${queryValues.length}`);
+    }
 
-		if (employee) {
-			queryValues.push(`%${employee}%`);
-			whereExpressions.push(`u.display_name ILIKE $${queryValues.length}`);
-		}
+    if (employee) {
+      queryValues.push(`%${employee}%`);
+      whereExpressions.push(`u.display_name ILIKE $${queryValues.length}`);
+    }
 
-		if (tableNum) {
-			queryValues.push(tableNum);
-			whereExpressions.push(`c.table_num = $${queryValues.length}`);
-		}
+    if (tableNum) {
+      queryValues.push(tableNum);
+      whereExpressions.push(`c.table_num = $${queryValues.length}`);
+    }
 
-		if (numGuests) {
-			queryValues.push(numGuests);
-			whereExpressions.push(`c.num_guests = $${queryValues.length}`);
-		}
+    if (numGuests) {
+      queryValues.push(numGuests);
+      whereExpressions.push(`c.num_guests = $${queryValues.length}`);
+    }
 
-		if (customer) {
-			queryValues.push(`%${customer}%`);
-			whereExpressions.push(`c.customer ILIKE $${queryValues.length}`);
-		}
+    if (customer) {
+      queryValues.push(`%${customer}%`);
+      whereExpressions.push(`c.customer ILIKE $${queryValues.length}`);
+    }
 
-		if (createdAt) {
-			queryValues.push(createdAt);
-			whereExpressions.push(`c.created_at = $${queryValues.length}`);
-		}
+    if (createdAt) {
+      queryValues.push(createdAt);
+      whereExpressions.push(`c.created_at = $${queryValues.length}`);
+    }
 
-		if (printedAt) {
-			queryValues.push(printedAt);
-			whereExpressions.push(`c.printed_at = $${queryValues.length}`);
-		}
+    if (printedAt) {
+      queryValues.push(printedAt);
+      whereExpressions.push(`c.printed_at = $${queryValues.length}`);
+    }
 
-		if (closedAt) {
-			queryValues.push(closedAt);
-			whereExpressions.push(`c.closed_at = $${queryValues.length}`);
-		}
+    if (closedAt) {
+      queryValues.push(closedAt);
+      whereExpressions.push(`c.closed_at = $${queryValues.length}`);
+    }
 
-		if (discountId) {
-			queryValues.push(discountId);
-			whereExpressions.push(`c.discount_id = $${queryValues.length}`);
-		}
+    if (discountId) {
+      queryValues.push(discountId);
+      whereExpressions.push(`c.discount_id = $${queryValues.length}`);
+    }
 
-		if (isVoid) {
-			queryValues.push(isVoid);
-			whereExpressions.push(`c.is_void = $${queryValues.length}`);
-		}
+    if (isVoid !== undefined) {
+      queryValues.push(isVoid);
+      whereExpressions.push(`c.is_void = $${queryValues.length}`);
+    }
 
-		if (whereExpressions.length > 0) {
-			query += ' WHERE ' + whereExpressions.join(' AND ');
-		}
+    if (whereExpressions.length > 0) {
+      query += ' WHERE ' + whereExpressions.join(' AND ');
+    }
 
-		// Finalize query and return results
+    // Finalize query and return results
 
-		query += ' ORDER BY c.created_at';
-		const checkRes = await db.query(query, queryValues);
-		return checkRes.rows;
-	}
+    query += ' ORDER BY c.created_at';
+    const checkRes = await db.query(query, queryValues);
+    return checkRes.rows;
+  }
 
-	/** Given a check id, return info about check.
+  /** Given a check id, return info about check.
      *
      * Returns { id, userId, employee, tableNum, numGuests, customer, createdAt, printedAt, closedAt, discountId, subtotal, discountTotal, localTax, stateTax, federalTax, isVoid }
      *
@@ -171,9 +171,9 @@ class Check {
      * 
      **/
 
-	static async get(id) {
-		const checkRes = await db.query(
-			`SELECT c.id,
+  static async get(id) {
+    const checkRes = await db.query(
+      `SELECT c.id,
               c.user_id AS "userId",
               u.display_name AS "employee",
               c.table_num AS "tableNum",
@@ -192,17 +192,17 @@ class Check {
       FROM checks c INNER JOIN users u
       ON c.user_id = u.id
       WHERE c.id = $1`,
-			[ id ]
-		);
+      [ id ]
+    );
 
-		const check = checkRes.rows[0];
+    const check = checkRes.rows[0];
 
-		if (!check) throw new NotFoundError(`No check: ${id}`);
+    if (!check) throw new NotFoundError(`No check: ${id}`);
 
-		return check;
-	}
+    return check;
+  }
 
-	/** Update check name with `data`.
+  /** Update check name with `data`.
    *
    * This is a "partial update" --- it's fine if data doesn't contain all the
    * fields; this only changes provided ones.
@@ -214,23 +214,23 @@ class Check {
    * Throws NotFoundError if not found.
    */
 
-	static async update(id, data) {
-		const { setCols, values } = sqlForPartialUpdate(data, {
-			tableNum: 'table_num',
-			numGuests: 'num_guests',
-			customer: 'customer',
-			printedAt: 'printed_at',
-			closedAt: 'closed_at',
-			discountId: 'discount_id',
-			discountTotal: 'discount_total',
-			localTax: 'local_tax',
-			stateTax: 'state_tax',
-			federalTax: 'federal_tax',
-			isVoid: 'is_void'
-		});
-		const checkVarIdx = '$' + (values.length + 1);
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      tableNum: 'table_num',
+      numGuests: 'num_guests',
+      customer: 'customer',
+      printedAt: 'printed_at',
+      closedAt: 'closed_at',
+      discountId: 'discount_id',
+      discountTotal: 'discount_total',
+      localTax: 'local_tax',
+      stateTax: 'state_tax',
+      federalTax: 'federal_tax',
+      isVoid: 'is_void'
+    });
+    const checkVarIdx = '$' + (values.length + 1);
 
-		const querySql = `UPDATE checks 
+    const querySql = `UPDATE checks 
                       SET ${setCols} 
                       WHERE id = ${checkVarIdx} 
                       RETURNING id,
@@ -244,15 +244,15 @@ class Check {
                       state_tax AS "stateTax",
                       federal_tax AS "federalTax",
                       is_void AS "isVoid"`;
-		const result = await db.query(querySql, [ ...values, id ]);
-		const check = result.rows[0];
+    const result = await db.query(querySql, [ ...values, id ]);
+    const check = result.rows[0];
 
-		if (!check) throw new NotFoundError(`No check: ${id}`);
+    if (!check) throw new NotFoundError(`No check: ${id}`);
 
-		return check;
-	}
+    return check;
+  }
 
-	/** Delete given check from database; returns undefined.
+  /** Delete given check from database; returns undefined.
    *
    * Throws NotFoundError if check not found.
    * 
@@ -260,18 +260,18 @@ class Check {
    * check should be voided instead is_void=true
    **/
 
-	static async remove(id) {
-		const result = await db.query(
-			`DELETE
+  static async remove(id) {
+    const result = await db.query(
+      `DELETE
        FROM checks
        WHERE id = $1
        RETURNING id`,
-			[ id ]
-		);
-		const check = result.rows[0];
+      [ id ]
+    );
+    const check = result.rows[0];
 
-		if (!check) throw new NotFoundError(`No check: ${id}`);
-	}
+    if (!check) throw new NotFoundError(`No check: ${id}`);
+  }
 }
 
 module.exports = Check;
--- models/payment.js
+++ models/payment.js
@@ -13,7 +13,7 @@ const { sqlForPartialUpdate } = require('../helpers/sql');
  */
 
 class Payment {
-	/** Create a payment (from data), update db, return new item data.
+  /** Create a payment (from data), update db, return new item data.
    *
    * data should be { check_id, type, tip_amt, subtotal }
    *
@@ -21,20 +21,20 @@ class Payment {
    *
    * */
 
-	static async create({ checkId, type, tipAmt, subtotal }) {
-		const result = await db.query(
-			`INSERT INTO payments
+  static async create({ checkId, type, subtotal, tipAmt }) {
+    const result = await db.query(
+      `INSERT INTO payments
            (check_id, type, tip_amt, subtotal)
            VALUES ($1, $2, $3, $4)
            RETURNING check_id AS "checkId", type, tip_amt AS "tipAmt", subtotal, is_void AS "isVoid"`,
-			[ checkId, type, tipAmt, subtotal ]
-		);
-		const payment = result.rows[0];
+      [ checkId, type, tipAmt, subtotal ]
+    );
+    const payment = result.rows[0];
 
-		return payment;
-	}
+    return payment;
+  }
 
-	/** Find all payments (optional filter on searchFilters).
+  /** Find all payments (optional filter on searchFilters).
    *
    * searchFilters (all optional):
    * - type
@@ -43,58 +43,58 @@ class Payment {
    * Returns [ { id, checkId, type, tipAmt, subtotal, isVoid }...]
    * */
 
-	static async findAll(searchFilters = {}) {
-		let query = `SELECT id,
+  static async findAll(searchFilters = {}) {
+    let query = `SELECT id,
                         check_id AS "checkId",
                         type,
                         tip_amt AS "tipAmt",
                         subtotal,
                         is_void AS "isVoid"
                  FROM payments`;
-		let whereExpressions = [];
-		let queryValues = [];
+    let whereExpressions = [];
+    let queryValues = [];
 
-		const { checkId, type, isVoid } = searchFilters;
+    const { checkId, type, isVoid } = searchFilters;
 
-		// For each possible search term, add to whereExpressions and queryValues so
-		// we can generate the right SQL
+    // For each possible search term, add to whereExpressions and queryValues so
+    // we can generate the right SQL
 
-		if (checkId) {
-			queryValues.push(checkId);
-			whereExpressions.push(`check_id = $${queryValues.length}`);
-		}
+    if (checkId) {
+      queryValues.push(checkId);
+      whereExpressions.push(`check_id = $${queryValues.length}`);
+    }
 
-		if (type) {
-			queryValues.push(type);
-			whereExpressions.push(`type = $${queryValues.length}`);
-		}
+    if (type) {
+      queryValues.push(type);
+      whereExpressions.push(`type = $${queryValues.length}`);
+    }
 
-		if (isVoid !== undefined) {
-			queryValues.push(isVoid);
-			whereExpressions.push(`is_void = $${queryValues.length}`);
-		}
+    if (isVoid !== undefined) {
+      queryValues.push(isVoid);
+      whereExpressions.push(`is_void = $${queryValues.length}`);
+    }
 
-		if (whereExpressions.length > 0) {
-			query += ' WHERE ' + whereExpressions.join(' AND ');
-		}
+    if (whereExpressions.length > 0) {
+      query += ' WHERE ' + whereExpressions.join(' AND ');
+    }
 
-		// Finalize query and return results
+    // Finalize query and return results
 
-		query += ' ORDER BY check_id';
-		const paymentsRes = await db.query(query, queryValues);
-		return paymentsRes.rows;
-	}
+    query += ' ORDER BY check_id';
+    const paymentsRes = await db.query(query, queryValues);
+    return paymentsRes.rows;
+  }
 
-	/** Given a payment id, return data about the payment.
+  /** Given a payment id, return data about the payment.
      *
      * Returns { id, checkId, type, tipAmt, subtotal, isVoid }
      *
      * Throws NotFoundError if not found.
      **/
 
-	static async get(id) {
-		const paymentRes = await db.query(
-			`SELECT id,
+  static async get(id) {
+    const paymentRes = await db.query(
+      `SELECT id,
               check_id AS "checkId",
               type,
               tip_amt AS "tipAmt",
@@ -102,17 +102,17 @@ class Payment {
               is_void AS "isVoid"
         FROM payments
         WHERE id = $1`,
-			[ id ]
-		);
+      [ id ]
+    );
 
-		const payment = paymentRes.rows[0];
+    const payment = paymentRes.rows[0];
 
-		if (!payment) throw new NotFoundError(`No payment: ${id}`);
+    if (!payment) throw new NotFoundError(`No payment: ${id}`);
 
-		return payment;
-	}
+    return payment;
+  }
 
-	/** Update payment data with `data`.
+  /** Update payment data with `data`.
    *
    * This is a "partial update" --- it's fine if data doesn't contain all the
    * fields; this only changes provided ones.
@@ -124,15 +124,15 @@ class Payment {
    * Throws NotFoundError if not found.
    */
 
-	static async update(id, data) {
-		const { setCols, values } = sqlForPartialUpdate(data, {
-			checkId: 'check_id',
-			tipAmt: 'tip_amt',
-			isVoid: 'is_void'
-		});
-		const paymentVarIdx = '$' + (values.length + 1);
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      checkId: 'check_id',
+      tipAmt: 'tip_amt',
+      isVoid: 'is_void'
+    });
+    const paymentVarIdx = '$' + (values.length + 1);
 
-		const querySql = `UPDATE payments 
+    const querySql = `UPDATE payments 
                         SET ${setCols} 
                         WHERE id = ${paymentVarIdx} 
                         RETURNING id, 
@@ -141,33 +141,33 @@ class Payment {
                                   tip_amt AS "tipAmt",
                                   subtotal, 
                                   is_void AS "isVoid"`;
-		const result = await db.query(querySql, [ ...values, id ]);
-		const payment = result.rows[0];
+    const result = await db.query(querySql, [ ...values, id ]);
+    const payment = result.rows[0];
 
-		if (!payment) throw new NotFoundError(`No payment: ${id}`);
+    if (!payment) throw new NotFoundError(`No payment: ${id}`);
 
-		return payment;
-	}
+    return payment;
+  }
 
-	/** Delete given payment from database; returns undefined.
+  /** Delete given payment from database; returns undefined.
    *
    * Throws NotFoundError if item not found.
    * 
    * This method should not be used: instead is_void=true
    **/
 
-	static async remove(id) {
-		const result = await db.query(
-			`DELETE
+  static async remove(id) {
+    const result = await db.query(
+      `DELETE
              FROM payments
              WHERE id = $1
              RETURNING id`,
-			[ id ]
-		);
-		const payment = result.rows[0];
+      [ id ]
+    );
+    const payment = result.rows[0];
 
-		if (!payment) throw new NotFoundError(`No payment: ${id}`);
-	}
+    if (!payment) throw new NotFoundError(`No payment: ${id}`);
+  }
 }
 
 module.exports = Payment;
