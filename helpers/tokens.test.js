37a2ceb63fd98c5874b33d7d243d28dce2653372
Kep <KepCreations@gmail.com>
2022-04-13 22:37:14 -0400

    Update tokens test to reflect token change: include exp

--- helpers/tokens.test.js
+++ helpers/tokens.test.js
@@ -3,34 +3,37 @@ const { createToken } = require('./tokens');
 const { SECRET_KEY } = require('../config');
 
 describe('createToken', function() {
-	test('works: not admin', function() {
-		const token = createToken({ username: 'test', roleId: 1 });
-		const payload = jwt.verify(token, SECRET_KEY);
-		expect(payload).toEqual({
-			iat: expect.any(Number),
-			username: 'test',
-			roleId: 1
-		});
-	});
+  test('works: not admin', function() {
+    const token = createToken({ username: 'test', role: 'trainee' });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+      username: 'test',
+      role: 'trainee'
+    });
+  });
 
-	test('works: admin', function() {
-		const token = createToken({ username: 'test', roleId: 10 });
-		const payload = jwt.verify(token, SECRET_KEY);
-		expect(payload).toEqual({
-			iat: expect.any(Number),
-			username: 'test',
-			roleId: 10
-		});
-	});
+  test('works: admin', function() {
+    const token = createToken({ username: 'test', role: 'manager' });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+      username: 'test',
+      role: 'manager'
+    });
+  });
 
-	test('works: default no admin', function() {
-		// given the security risk if this didn't work, checking this specifically
-		const token = createToken({ username: 'test' });
-		const payload = jwt.verify(token, SECRET_KEY);
-		expect(payload).toEqual({
-			iat: expect.any(Number),
-			username: 'test',
-			roleId: 1
-		});
-	});
+  test('works: default no admin', function() {
+    // given the security risk if this didn't work, checking this specifically
+    const token = createToken({ username: 'test' });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+      username: 'test',
+      role: 'trainee'
+    });
+  });
 });
